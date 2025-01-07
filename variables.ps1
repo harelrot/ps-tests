51433c9bb5aac96815f960d8251a67e456600efb
Harel Rot (one support centers ltd) <v-harelrot@microsoft.com>
2025-01-07 15:49:07 +0200

    init

---
+++ variables.ps1
@@ -0,0 +1,11 @@
+# Define some variables
+$greeting = "Hello, World!"
+$number = 42
+$isPowerShellAwesome = $true
+$currentDate = Get-Date
+
+# Output the variables
+Write-Output $greeting
+Write-Output $number
+Write-Output $isPowerShellAwesome
+Write-Output $currentDate
